14515412084f98ff1afea989d568a77eca5d8c9f
ypraw <dyo.ndutz@gmail.com>
2017-06-02 15:59:08 +0700

    Source Code Updated Jumat, 02 Juni 2017, 15:59 wib

--- webFlask/database.py
+++ webFlask/database.py
@@ -1,24 +1,55 @@
 import MySQLdb as mysql
 import hashlib
+import sys
+import warnings
 
 
-def computeMD5hash(string):
-    m = hashlib.md5()
-    m.update(string.encode('utf-8'))
-    return m.hexdigest()
+class MysqlUserDB:
+    warnings
+    warnings.filterwarnings('error')
 
+    def __init__(self, DBrootHost, DBrootUser, DBrootPass, DBrootDatabase):
+        self.DBrootHost = DBrootHost
+        self.DBrootUser = DBrootUser
+        self.DBrootPass = DBrootPass
+        self.DBrootDatabase = DBrootDatabase
 
-db = mysql.connect("127.0.0.1", "root", "", "flasktutorial")
-cur = db.cursor()
+        try:
+            print("Checking connection of MYSQL ...")
+            self.con = mysql.connect(DBrootHost, DBrootUser, DBrootPass, DBrootDatabase)
+            self.cursor = self.con.cursor()
+            self.cursor.execute('Select version()')
+            print("Connected to Mysql Database")
+        except MySQLdb.Error as error:
+            print("Error %s\n Stop.\n" % error)
+            sys.exit()
 
-cur.execute("Select * from tb_user")
-row = cur.fetchone()[4]
+    def CreateDB(self, DBrootHost):
+        print("Creating database...")
+        try:
+            self.cursor.execute('create database if not exists ' + DBrootHost)
+            self.cursor.execute('show databases like %s' % DBrootHost)
+            dbs = self.cursor.fetchone()
+            print('Database created: %s' % dbs)
+        except Warning as warn:
+            print("Warning: %s \nStop.\n" % warn)
+            sys.exit()
 
-if (computeMD5hash('admina') == row):
-    print("yeay")
-    print(row)
-else:
-    print("this Fucking shit")
+    def GrantsAccess(self, DBrootUser, DBrootPass, DBrootDatabase):
+        print("Accesing Account ...")
+        try:
+            self.cursor.execute("SELECT user,db FROM mysql.db WHERE db ='%s'"
+                                % (DBrootDatabase))
+            result = self.cursor.fetchone()
+            print("Access Granted", result)
+        except Warning as warn:
+            print("Warning %s" % warn)
+        except mysql.Error as error:
+            print("error %s \n stop" % error)
+            sys.exit()
 
-cur.close()
-db.close()
+    def __del__(self):
+        print("Finishing operation ...")
+        self.cursor.close()
+        self.con.close()
+        print("Finished")
---
+++ webFlask/testdb.py
@@ -0,0 +1,6 @@
+import database as dbs
+
+mysqli = dbs.MysqlUserDB(DBrootHost='127.0.0.1', DBrootUser='root',
+                         DBrootPass='', DBrootDatabase='flasktutorial')
+mysqli.GrantsAccess(DBrootUser='root', DBrootPass='',
+                    DBrootDatabase='flasktutorial')
