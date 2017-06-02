9e6791d274a8989048ad5881d5f74cc6ab8a1559
ypraw <dyo.ndutz@gmail.com>
2017-06-02 16:00:14 +0700

    Source Code Updated Jumat, 02 Juni 2017, 16:00 wib

--- webFlask/testdb.py
+++ webFlask/testdb.py
@@ -4,3 +4,4 @@ mysqli = dbs.MysqlUserDB(DBrootHost='127.0.0.1', DBrootUser='root',
                          DBrootPass='', DBrootDatabase='flasktutorial')
 mysqli.GrantsAccess(DBrootUser='root', DBrootPass='',
                     DBrootDatabase='flasktutorial')
+del mysqli
