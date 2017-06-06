d7c0ced6869e12824054a7a533e5771bb7a02576
ypraw <dyo.ndutz@gmail.com>
2017-06-06 23:35:06 +0700

    Source Code Updated Selasa, 06 Juni 2017, 23:35 wib

--- webFlask/testdb.py
+++ webFlask/testdb.py
@@ -9,8 +9,8 @@ mysqli.GrantsAccess("flasktutorial")
 db = mysqli.getDB()
 paswd = mysqli.computeMD5hash("admin")
 try:
-    db.execute("INSERT INTO tb_user VALUES(%s,%s,%s,%s,%s,%s)",
-               ('2', 'mugeni', 'yeay', 'user1', paswd, "mugeni@gmail.com"))
+    db.execute("INSERT INTO tb_user VALUES(?,%s,%s,%s,%s,%s)",
+               ('', 'anu', 'yeay', 'user1', paswd, "mugeni@gmail.com"))
 except Warning as warn:
     print("attention %s", warn)
 mysqli.delCon()
---
+++ webFlask/views5.py
@@ -0,0 +1,57 @@
+#!flask/bin/python
+
+from flask import Flask, render_template, request, url_for, redirect, session
+import connection as db
+
+app = Flask(__name__, static_url_path='')
+
+konek = db.MysqlUserDB('127.0.0.1', 'root', '', 'flasktutorial')
+cur = konek.getDB()
+
+
+@app.route('/')
+def index():
+    return render_template('home5.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if validasi_login(request.form['username'], konek.computeMD5hash(request.form['password'])):
+            session['username'] = request.form.get('username')
+            return redirect(url_for('dashboard'))
+        else:
+            error = "incorrect password or username"
+    return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for("index"))
+
+
+def validasi_login(username, password):
+    cur.execute("SELECT * from tb_user WHERE username=%s AND password=%s",
+                (username, password))
+    result = cur.fetchone()
+    if result:
+        return True
+    else:
+        return False
+
+
+@app.route('/dashboard')
+def dashboard():
+    if 'username' in session:
+        return render_template('dashboard5.html', username=session['username'])
+    else:
+        return redirect(url_for('index'))
+
+
+#   End Bagian keempat
+# if __name__ == '__main__':
+app.debug = True
+app.secret_key = '4KuC1nT@K4MU'
+app.run()
