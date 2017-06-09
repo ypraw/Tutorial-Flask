e9f5f0900c9ba5d93413c4fb530f19d233da0998
ypraw <dyo.ndutz@gmail.com>
2017-06-10 04:59:01 +0700

    Source Code Updated Sabtu, 10 Juni 2017, 04:59 wib

--- webFlask/testdb.py
+++ webFlask/testdb.py
@@ -3,23 +3,16 @@ import warnings
 warnings.filterwarnings('error')
 
 mysqli = dbs.MysqlUserDB(DBrootHost='127.0.0.1', DBrootUser='root',
-                         DBrootPass='', DBrootDatabase='flasktutorial')
-# mysqli.CreateDB(DBrootDatabase='nganu1')
-# mysqli.GrantsAccess("flasktutorial")
+                         DBrootPass='', DBrootDatabase='pos')
+
 db = mysqli.getDB()
-# 900150983cd24fb0d6963f7d28e17f72
-# 0cc175b9c0f1b6a831c399e269772661
 
-# try:
-#    db.execute("INSERT INTO tb_user VALUES(?,%s,%s,%s,%s,%s)",
-#               ('', 'anu', 'yeay', 'user1', paswd, "mugeni@gmail.com"))
-# except Warning as warn:
-#    print("attention %s", warn)
-
+# Create pass admin in md5
+paswd = mysqli.computeMD5hash("admin")
 try:
-    db.execute("select * from tb_content")
-    result = db.fetchone()
-    print(result)
-except db.Error as error:
-    print(error)
+    db.execute("INSERT INTO tb_user VALUES(%s,%s,%s,%s,%s,%s)",
+               (None, 'anu', 'yeay', 'user2', paswd, "mugeni@gmail.com"))
+except Warning as warn:
+    print("attention %s", warn)
+
 mysqli.delCon()
