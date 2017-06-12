142b9c090482657e90f1ff08f4b88b06c6999bbe
ypraw <dyo.ndutz@gmail.com>
2017-06-13 01:47:04 +0700

    Source Code Updated Selasa, 13 Juni 2017, 01:47 wib

--- flask-alchemy/pos/models/login.py
+++ flask-alchemy/pos/models/login.py
@@ -8,3 +8,24 @@ class TB_user(db.Model):
     username = db.Column(db.String(50), nullable=False)
     password = db.Column(db.String(120), nullable=False)
     email = db.Column(db.String(50), nullable=False)
+
+    def __init__(self, username, password, email):
+        self.username = username
+        self.password = password
+        self.email = email
+        self.registered_on = datetime.utcnow()
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return unicode(self.id)
+
+    def __repr__(self):
+        return '<User %r>' % (self.username)
--- flask-alchemy/pos/views/login.py
+++ flask-alchemy/pos/views/login.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, render_template, request, redirect
-#from pos.models import db
-#from pos.models.login import TB_user
+from flask import Blueprint, render_template, request, redirect, url_for
+from pos.models import db
+from pos.models.login import TB_user
 
 bp = Blueprint("login", __name__)
 
@@ -9,4 +9,19 @@ bp = Blueprint("login", __name__)
 
 @bp.route("/login")
 def login():
-    return render_template("login.html")
+    # Jika belum login maka di arah ke login.html
+    if request.method == 'GET':
+        return render_template('login.html')
+    # Jika sudah login
+    return redirect(url_for('product'))
+
+
+def register():
+    if request.method == 'GET':
+        return render_template('register.html')
+    user = TB_user(request.form['username'], request.form['password'],
+                   request.form['email'])
+    db.session.add(user)
+    db.session.commit()
+    # flash('User successfully registered')
+    return redirect(url_for('login'))
